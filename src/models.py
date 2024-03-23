b272d8ec79b0e4235fa010f3741abf9dd40e827c
Prayuj Tuli <prayujtuli@hotmail.com>
2024-03-23 12:46:14 -0400

    created tables

--- src/models.py
+++ src/models.py
@@ -1,33 +1,36 @@
 import os
 from dotenv import dotenv_values
-import dotenv
 import datetime
 from peewee import Model, TextField, DateTimeField, PostgresqlDatabase, UUIDField
 
 config = dotenv_values('../.env')
-# TODO: Create a database
 db = PostgresqlDatabase(
-        config['POSTGRES_DB_NAME'], 
-        user=config['POSTGRES_USERNAME'], 
-        password=config['POSTGRES_PASSWORD'], 
+        config['POSTGRES_DB_NAME'],
+        user=config['POSTGRES_USERNAME'],
+        password=config['POSTGRES_PASSWORD'],
         host=config['POSTGRES_HOST'])
 db.connect()
-                        
+
+
 class BaseModel(Model):
     class Meta:
         database = db
+
 
 class Club(BaseModel):
     club_id = UUIDField()
     club_name = TextField()
 
+
 class Affiliation(BaseModel):
     username = TextField()
     club_id = UUIDField()
 
+
 class User(BaseModel):
     username = TextField()
     password = TextField()
+
 
 class Post(BaseModel):
     post_id = UUIDField()
@@ -36,12 +39,14 @@ class Post(BaseModel):
     content = TextField()
     created_at = DateTimeField(default=datetime.datetime.now)
 
+
 class Comments(BaseModel):
     comment_id = UUIDField()
     post_id = UUIDField()
     author = TextField()
     content = TextField()
     created_at = DateTimeField(default=datetime.datetime.now)
+
 
 class Event(BaseModel):
     event_id = UUIDField()
@@ -52,8 +57,11 @@ class Event(BaseModel):
     created_at = DateTimeField(default=datetime.datetime.now)
     event_date = DateTimeField()
 
+
 class Interested(BaseModel):
     event_id = UUIDField()
     club_id = UUIDField()
     interestee = TextField()
 
+
+db.create_tables([Club, Affiliation, User, Post, Comments, Event, Interested])
