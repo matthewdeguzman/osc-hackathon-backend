cfb88d762921c635c30a113c98b783d1c4acdf70
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-03-23 12:40:53 -0400

    Add models.py

--- src/main.py
+++ src/main.py
@@ -2,5 +2,6 @@ from typing import Annotated
 
 from fastapi import Body, FastAPI
 from pydantic import BaseModel, Field
+import models
 
 app = FastAPI()
---
+++ src/models.py
@@ -0,0 +1,59 @@
+import os
+from dotenv import dotenv_values
+import dotenv
+import datetime
+from peewee import Model, TextField, DateTimeField, PostgresqlDatabase, UUIDField
+
+config = dotenv_values('../.env')
+# TODO: Create a database
+db = PostgresqlDatabase(
+        config['POSTGRES_DB_NAME'], 
+        user=config['POSTGRES_USERNAME'], 
+        password=config['POSTGRES_PASSWORD'], 
+        host=config['POSTGRES_HOST'])
+db.connect()
+                        
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Club(BaseModel):
+    club_id = UUIDField()
+    club_name = TextField()
+
+class Affiliation(BaseModel):
+    username = TextField()
+    club_id = UUIDField()
+
+class User(BaseModel):
+    username = TextField()
+    password = TextField()
+
+class Post(BaseModel):
+    post_id = UUIDField()
+    author = TextField()
+    title = TextField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+class Comments(BaseModel):
+    comment_id = UUIDField()
+    post_id = UUIDField()
+    author = TextField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+class Event(BaseModel):
+    event_id = UUIDField()
+    club_id = UUIDField()
+    author = TextField()
+    title = TextField()
+    description = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+    event_date = DateTimeField()
+
+class Interested(BaseModel):
+    event_id = UUIDField()
+    club_id = UUIDField()
+    interestee = TextField()
+
